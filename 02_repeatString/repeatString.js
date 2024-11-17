1309b24568a9736fbf1a2a423257a6753d4d3433
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 17:00:06 -0500

    Unskip all tests and add case for num < 0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(string,num) {
     let finalString = "";
+    if(num < 0){
+        return "ERROR";
+    }
     for(let i = 0; i < num; i++){
         finalString+=string;
     }
