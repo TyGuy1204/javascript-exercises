e4ed450b2750bf0b61ca9b9c02dcc792a82446da
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 17:23:37 -0500

    Use splice and indexOf to remove element

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,num) {
+    let numLocation  = arr.indexOf(num);
+    let remElement = arr.splice(numLocation,1);
+    console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
