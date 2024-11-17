ecb8b63b74b9641944084529f582359f802945e3
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 17:05:20 -0500

    Add return of reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let curString  = string;
+    let reversedString  = "";
+    for(let i = 0; i < string.length; i++){
+        reversedString += curString.substr(curString.length-1);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
