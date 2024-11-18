a4a5cbc38b853d87308ad19b9519df153f6e0e42
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 19:54:30 -0500

    Use while loop instead to encompass multiple of same value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
 const removeFromArray = function(arr, ...args) {
+    let temp = 0;
     for(let i = 0; i < args.length; i++){
-        arr.splice(arr.indexOf(args[i]),1);
+       while(arr.includes(args[i])){
+        temp = arr.splice(arr.indexOf(args[i]),1);
+       }
     }
     return arr;
 };
