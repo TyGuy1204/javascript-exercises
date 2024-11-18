3cac93d02f140d1d530b48402c264be3fffada18
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 22:01:03 -0500

    Account for rounding and formatting

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
 const convertToCelsius = function(temp) {
-  return (((temp -32)*5)/9);
+  if((((temp -32)*5)/9) % 1 !== 0){
+    return parseFloat((((temp -32)*5)/9).toFixed(1));
+  }
+  else{
+    return parseFloat(((temp -32)*5)/9);
+  }
+  
 };
 
 const convertToFahrenheit = function(temp) {
-  return (((temp * 9)/5)+32);
+  if((((temp * 9)/5)+32) % 1 !== 0){
+    return parseFloat((((temp * 9)/5)+32).toFixed(1));
+  }
+  else{
+    return parseFloat(((temp * 9)/5)+32);
+  }
+  
 };
 
 // Do not edit below this line
