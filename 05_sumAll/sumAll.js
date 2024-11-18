05ff3a6b802be5519b9e20a1d843b33b98af8261
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 20:58:43 -0500

    Finished case for num2 < num1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,26 +1,28 @@
 const sumAll = function(num1,num2) {
     let sum  = 0;
-    if((Number.isInteger(num1) && Number.isInteger(num2))!=true){
+    if((Number.isInteger(num1) && Number.isInteger(num2))!=true || (!(num1>=0) || !(num2>=0))){
         return "ERROR";
     }
-    if(!(num1>=0) || !(num2>=0)){
-        return "ERROR";
-    }
-    if(num1 == num2){
-        return num1;
-    }
-    else if(num1 < num2){
-        for(let i  = num1; i <= num2; i++){
-            sum+=i;
+    else{
+        
+        if(num1 == num2){
+            return num1;
         }
-    }
-    else if(num1 > num2){
-        for(let i  = num2; i >= num1; i--){
-            sum+=i;
+        else if(num1 < num2){
+            for(let i  = num1; i <= num2; i++){
+                sum+=i;
+            }
         }
+        else if(num2 < num1){
+            console.log("Entered conditional");
+            for(let j = num2; j <= num1; j++){
+                console.log("Entered loop");
+                sum += j;
+            }
+        }
+        return sum;
     }
-    return sum;
-};
+}
 
 // Do not edit below this line
 module.exports = sumAll;
