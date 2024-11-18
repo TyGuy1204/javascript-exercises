8618b08b82ce64f4c71cbb6aa32443709c6aaf94
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 20:01:52 -0500

    Add case for num2 > num1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,9 +3,17 @@ const sumAll = function(num1,num2) {
     if(num1 == num2){
         return num1;
     }
-    for(let i  = num1; i <= num2; i++){
-        sum+=i;
+    if(num1 < num2){
+        for(let i  = num1; i <= num2; i++){
+            sum+=i;
+        }
     }
+    else if(num1 > num2){
+        for(let i  = num2; i >= num1; i--){
+            sum+=i;
+        }
+    }
+    
     return sum;
 };
 
