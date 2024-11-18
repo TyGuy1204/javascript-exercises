70d0691ca0ec6b1783ac47f80649e66588ba8669
Tyler Scott <tylerclaytonscott@gmail.com>
2024-11-17 20:10:03 -0500

    Add case for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,9 @@ const sumAll = function(num1,num2) {
     if((Number.isInteger(num1) && Number.isInteger(num2))!=true){
         return "ERROR";
     }
+    if(!(num1>=0) || !(num2>=0)){
+        return "ERROR";
+    }
     if(num1 == num2){
         return num1;
     }
